c4c2645a83f2c4215e4826a0f962752a4b3d780d
Marina Hernandez <marina.hernandez@lecol-044.home>
2023-04-26 17:49:19 +0100

    Eliminar ficheros

--- proyecto.js
+++ proyecto.js
@@ -5,10 +5,6 @@ const button = document.getElementById('localizacion');
 const resultado = document.getElementById('resultado');
 const container = document.getElementById('contenido');
 
-//Establecemos las variables latitud y longitud (Es posible que debamos borrarlas y ponerlas dentro del if)
-// let latitude;
-// let longitude;
-
 //Creamos un evento asociado al click en el botón
 button.addEventListener('click', () => {
   //Dspués del click, si el navegador accede a la geolocalización nos devuelve la posición mediante lat y lon
@@ -25,7 +21,6 @@ button.addEventListener('click', () => {
 
         //Comprobamos que podemos ver las propiedades de la API
         console.log(url);
-        // resultado.textContent = `Latitud: ${latitude}, Longitud: ${longitude}`;
 
         // Hacemos las peticiones a la API mediante el método Fetch
         fetch(url)
@@ -40,18 +35,21 @@ button.addEventListener('click', () => {
             console.log(city);
             resultado.textContent = `${city}`;
 
-            //Separamos las 8 primeras horas del array y las almacenamos en la variable datosHoras
-            // const datosHoras = data.data.slice(0, 8);
-            // console.log(datosHoras);
+            //Prueba de formateo de hora
+            const arrHoras = data.data.map((datos) => datos.timestamp_local);
+            console.log(arrHoras);
 
-            //Obtenemos un array con los datos de lluvia (Esto es una prueba, pero funciona, falta interpretar esos datos)
-            //La probabilidad de lluvia se mide con pop o conprecip (no tenemos ni puta idea)
-            //ESto hay que revisarlo porque no funciona del todo bien (de la linea 50 a la 54)
-            const problluvia = data.data.map((datos) => datos.precip);
-            console.log(problluvia);
+            //Aquí hacemos un bucle que recorra los datos de la lluvia en cada hora evaluando su probabilidad
+            const problluvia = data.data.map((datos) => datos.pop * 100);
 
-            let lluvia = problluvia >= 0.5 ? 'Llueve' : 'No llueve';
-            console.log(lluvia);
+            for (let i = 0; i < problluvia.length; i++) {
+              const hora = new Date(arrHoras[i]).toLocaleTimeString('es-ES', {
+                hour: '2-digit',
+                minute: '2-digit',
+              });
+              const lluvia = problluvia[i] > 50 ? 'Llueve' : 'No llueve';
+              console.log(`A las ${hora} ${lluvia}`);
+            }
           })
           //Usamos el método Catch por si hay algún error obteniendo info de la API
           .catch((error) => {
@@ -69,20 +67,3 @@ button.addEventListener('click', () => {
     resultado.textContent = 'El navegador no soporta geolocalización';
   }
 });
-
-// const horaLluvia = (num) => {
-//   return num < 0.5 ? 'Llueve' : 'No llueve'
-// // 3. Función que recibe un valor numérico (una temperatura) y retorna un string con la clase
-// //    que corresponda.
-// const getProbLluvia = (temp) => {
-//     if (temp < 4) {
-//         return 'lower';
-//     } else if (temp < 20) {
-//         return 'low';
-//     } else if (temp < 30){
-//         return 'medium';d
-//     } else {
-//         return 'high';
-//     }
-// }
-// }
