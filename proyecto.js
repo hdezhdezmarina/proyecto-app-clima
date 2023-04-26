b6f138a603068816d7ea9d9bce8ff3ba1f4a4dae
HACKER <jorge.novo.rodriguez.93@gmail.com>
2023-04-26 22:10:50 +0200

    Dia 26

--- proyecto.js
+++ proyecto.js
@@ -3,14 +3,11 @@
 //Seleccionamos los elementos de HTML que vamos a usar mediante su ID
 const button = document.getElementById('localizacion');
 const resultado = document.getElementById('resultado');
-const container = document.getElementById('contenido');
-
-//Establecemos las variables latitud y longitud (Es posible que debamos borrarlas y ponerlas dentro del if)
-// let latitude;
-// let longitude;
+const contenido = document.getElementById('contenido');
+const listaOchoHoras = document.getElementById('lista');
 
 //Creamos un evento asociado al click en el botón
-button.addEventListener('click', () => {
+button.addEventListener('click', (event) => {
   //Dspués del click, si el navegador accede a la geolocalización nos devuelve la posición mediante lat y lon
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -24,34 +21,48 @@ button.addEventListener('click', () => {
         const url = `https://api.weatherbit.io/v2.0/forecast/hourly?lat=${latitude}&lon=${longitude}&key=${apiKey}&hours=8`;
 
         //Comprobamos que podemos ver las propiedades de la API
-        console.log(url);
-        // resultado.textContent = `Latitud: ${latitude}, Longitud: ${longitude}`;
-
         // Hacemos las peticiones a la API mediante el método Fetch
         fetch(url)
           .then((response) => response.json())
-          .then((data) => {
+          .then((body) => {
             //Console log para ver los datos de la API
-            console.log(data);
 
             //Establecemos una constante con los datos que queremos utilizar y comprobamos que funcione con console.log
             //También probamos a imprimir en el html
-            const city = data.city_name;
-            console.log(city);
+            const city = body.city_name;
             resultado.textContent = `${city}`;
 
-            //Separamos las 8 primeras horas del array y las almacenamos en la variable datosHoras
-            // const datosHoras = data.data.slice(0, 8);
-            // console.log(datosHoras);
+            //Aquí hacemos un bucle que recorra los datos de la lluvia en cada hora evaluando su probabilidad
+            const datosHoraYLluvia = body.data.map((datos) => {
+              return {
+                precipitacion: datos.weather.description,
+                horasLluvia: datos.timestamp_local,
+              };
+            });
 
-            //Obtenemos un array con los datos de lluvia (Esto es una prueba, pero funciona, falta interpretar esos datos)
-            //La probabilidad de lluvia se mide con pop o conprecip (no tenemos ni puta idea)
-            //ESto hay que revisarlo porque no funciona del todo bien (de la linea 50 a la 54)
-            const problluvia = data.data.map((datos) => datos.precip);
-            console.log(problluvia);
+            for (let i = 0; i < datosHoraYLluvia.length; i++) {
+              const hora = new Date(
+                datosHoraYLluvia[i].horasLluvia
+              ).toLocaleTimeString('es-ES', {
+                hour: '2-digit',
+                minute: '2-digit',
+              });
+              const lluvia = datosHoraYLluvia[i].precipitacion.includes(
+                'rain' || 'drizzle'
+              )
+                ? 'llueve'
+                : 'no llueve';
 
-            let lluvia = problluvia >= 0.5 ? 'Llueve' : 'No llueve';
-            console.log(lluvia);
+              //Imprimimos la lista de horas en HTML
+              const meteo = document.createElement('li');
+
+              meteo.innerHTML = `
+              <p>
+              A las ${hora} ${lluvia}.
+              </p>
+              `;
+              listaOchoHoras.appendChild(meteo);
+            }
           })
           //Usamos el método Catch por si hay algún error obteniendo info de la API
           .catch((error) => {
@@ -69,20 +80,3 @@ button.addEventListener('click', () => {
     resultado.textContent = 'El navegador no soporta geolocalización';
   }
 });
-
-// const horaLluvia = (num) => {
-//   return num < 0.5 ? 'Llueve' : 'No llueve'
-// // 3. Función que recibe un valor numérico (una temperatura) y retorna un string con la clase
-// //    que corresponda.
-// const getProbLluvia = (temp) => {
-//     if (temp < 4) {
-//         return 'lower';
-//     } else if (temp < 20) {
-//         return 'low';
-//     } else if (temp < 30){
-//         return 'medium';d
-//     } else {
-//         return 'high';
-//     }
-// }
-// }
