35ea17666315f3662376369b89c52c628ce0bc3b
HACKER <jorge.novo.rodriguez.93@gmail.com>
2023-04-27 21:04:25 +0200

    dia 27 post tutoria

--- proyecto.js
+++ proyecto.js
@@ -3,11 +3,13 @@
 //Seleccionamos los elementos de HTML que vamos a usar mediante su ID
 const button = document.getElementById('localizacion');
 const resultado = document.getElementById('resultado');
-const contenido = document.getElementById('contenido');
 const listaOchoHoras = document.getElementById('lista');
 
 //Creamos un evento asociado al click en el botón
-button.addEventListener('click', (event) => {
+button.addEventListener('click', () => {
+  //Hacemos desaparecer el botón después del primer click
+  button.style.display = 'none';
+
   //Dspués del click, si el navegador accede a la geolocalización nos devuelve la posición mediante lat y lon
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
@@ -16,30 +18,30 @@ button.addEventListener('click', (event) => {
         let longitude = position.coords.longitude;
 
         //Establecemos la URL de la API modificada con los datos de latitud y longitud.
-        //Podemos usar un template string para modificar el número de horas de la predicción (final de la url)
         const apiKey = 'cd6668e778614fef868940d1cf862875';
         const url = `https://api.weatherbit.io/v2.0/forecast/hourly?lat=${latitude}&lon=${longitude}&key=${apiKey}&hours=8`;
 
-        //Comprobamos que podemos ver las propiedades de la API
         // Hacemos las peticiones a la API mediante el método Fetch
         fetch(url)
           .then((response) => response.json())
           .then((body) => {
             //Console log para ver los datos de la API
 
-            //Establecemos una constante con los datos que queremos utilizar y comprobamos que funcione con console.log
-            //También probamos a imprimir en el html
+            //Establecemos una constante con el nombre de la localización para imprimir en el HTML
             const city = body.city_name;
             resultado.textContent = `${city}`;
 
-            //Aquí hacemos un bucle que recorra los datos de la lluvia en cada hora evaluando su probabilidad
+            //Aquí hacemos un map con las propiedades de la API que vamos a usar
             const datosHoraYLluvia = body.data.map((datos) => {
               return {
                 precipitacion: datos.weather.description,
                 horasLluvia: datos.timestamp_local,
+                temperatura: datos.temp,
               };
             });
 
+            //Creamos un bucle que recorra el objeto retornado y formatee los datos
+            //de forma que devuelva la información que nosotros queremos para cada hora
             for (let i = 0; i < datosHoraYLluvia.length; i++) {
               const hora = new Date(
                 datosHoraYLluvia[i].horasLluvia
@@ -47,6 +49,7 @@ button.addEventListener('click', (event) => {
                 hour: '2-digit',
                 minute: '2-digit',
               });
+
               const lluvia = datosHoraYLluvia[i].precipitacion.includes(
                 'rain' || 'drizzle'
               )
@@ -56,11 +59,20 @@ button.addEventListener('click', (event) => {
               //Imprimimos la lista de horas en HTML
               const meteo = document.createElement('li');
 
-              meteo.innerHTML = `
-              <p>
-              A las ${hora} ${lluvia}.
-              </p>
+              if (lluvia) {
+                meteo.innerHTML = `
+                  <p>A las ${hora} llueve.</p>
+                  <img src="./animated/rainy-6.svg" alt="">
+                  <p></p>
+                `;
+              } else {
+                meteo.innerHTML = `
+                <p>A las ${hora} no llueve.</p>
+                <img src="./animated/cloudy-day-3.svg" alt="">
+                <p></p>
               `;
+              }
+
               listaOchoHoras.appendChild(meteo);
             }
           })
